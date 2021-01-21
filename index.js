bec8a1b0cc29beaa0eea7be2b1b1ef1aa4a9af71
Your Name <you@example.com>
2021-01-21 10:33:27 -0600

    Created connection and added a table { Note:- using "then" for createTable is nessasary.}

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("object");
+const knex = require("knex")({
+  client: 'pg',
+  connection: 'postgres://postgres:pgsql_rishav@localhost:5432/pgsql-project-db'
+});
+knex.schema.createTable('user', (table) => {
+  table.increments('id')
+  table.string('name')
+  table.integer('age')
+})
+.then(() => {
+  console.log("require");
+});
