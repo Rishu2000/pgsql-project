3d5ced73705b73b871c33f5ce46a156726e6cae6
Your Name <you@example.com>
2021-01-21 11:10:46 -0600

    insert into user table.

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const knex = require("knex")({
   client: 'pg',
   connection: 'postgres://postgres:pgsql_rishav@localhost:5432/pgsql-project-db'
 });
-knex.schema.createTable('user', (table) => {
-  table.increments('id')
-  table.string('name')
-  table.integer('age')
-})
-.then(() => {
-  console.log("Created a table name 'user'.");
-});
+  knex('user')
+  .insert({ name: "Rishu3", age:45})
+  .then(() => {
+    console.log("Inserted into table.")
+  });
